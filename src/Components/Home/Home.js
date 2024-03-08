3c1cdee9eebf798f98eb0447126374bfb54c3c60
jobinthekkanath <jobinthekkanath11@gmail.com>
2024-03-08 16:55:05 +0530

    firstcommit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+import SingleColor from '../SingleColor/SingleColor'
+import Values from 'values.js'
+
+function Home() {
+
+    const [list, setlist] = useState(new Values("#f15025").all(10))
+
+    const [color,setColor] =useState('')
+    const[error,setError] =useState(false)
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        try{
+            let colors=new Values(color).all(10)
+            setlist(colors)
+        } catch(error){
+            setError(true)
+            console.log(error)
+        }
+    }
+
+  return (
+    <>
+    <section className='container'>
+        <h3>Color Generator</h3>
+        <form  onSubmit={handleSubmit}>
+            <input type='text' value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`} />
+            <button className='btn' type='submit'>Submit</button>
+        </form>
+    </section>
+    <section className='colors'>
+        {
+            list.map((color,index)=>(
+                <SingleColor key={index} {...color}  index={index} hexColor={color.hex} />
+            ))
+        }
+        
+    </section>
+    </>
+  )
+}
+
+export default Home
